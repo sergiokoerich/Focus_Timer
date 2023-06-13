89ec6455fee482573b8c302dca163e7e8ced0b31
Sérgio Koerich Pereira <129325112+sergiokoerich@users.noreply.github.com>
2023-06-13 14:46:24 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+//clean code
+//EcmaScript - 2015 - ES6 Modules
+
+import Controls from "./controls.js"
+import Timer from "./timer.js"
+import Sound from "./sounds.js"
+import Events from "./events.js"
+import {
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop,
+  minutesDisplay,
+  secondsDisplay,
+} from "./elements.js"
+
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
+})
+
+const timer = Timer({
+  minutesDisplay, 
+  secondsDisplay, 
+  resetControls: controls.reset
+})
+
+const sound = Sound()
+
+Events({controls, timer, sound})
